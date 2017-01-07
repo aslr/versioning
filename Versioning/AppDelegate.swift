16899bd9832bbbdb3edb03d1a6e694dded23e7d9
João Varela <joaocvarela@gmail.com>
2017-01-07 19:11:08 +0000

    Version 1

---
+++ Versioning/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Versioning
+//
+//  Created by João Varela on 07/01/2017.
+//  Copyright © 2017 João Varela. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
